e4d80c00ff8d3c4a770f082564ae695cae0c8b80
= <=>
2019-02-01 17:54:25 -0300

    Done

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -24,23 +24,35 @@ MainWindow::~MainWindow()
 void MainWindow::createMenus(){
     opMenu = menuBar()->addMenu(tr("Opções"));
     QAction *newAction = opMenu->addAction(tr("Novo Contato"));
-    newAction->
     connect(newAction, SIGNAL(triggered()),mainWidget,SLOT(showAddEntryDialog()));
-    QAction *editAction = opMenu->addAction(tr("Editar Contato"));
+    editAction = opMenu->addAction(tr("Editar Contato"));
     editAction->setEnabled(false);
     opMenu->addAction(editAction);
-    connect(editAction, SIGNAL(triggered()), mainWidget, SLOT(editEntry));
+    connect(editAction, SIGNAL(triggered()), mainWidget, SLOT(editEntry()));
 
     opMenu->addSeparator();
 
-    QAction *removeAction = new QAction(tr("Remover Contato"));
+    removeAction = new QAction(tr("Remover Contato"));
     removeAction->setEnabled(false);
     opMenu->addAction(removeAction);
     connect(removeAction, SIGNAL(triggered()), mainWidget, SLOT(removeEntry()));
 
-    connect(mainWidget, SIGNAL(selectionChanged()),
-        this, SLOT(updateActions()));
+    connect(mainWidget, &AddressWidget::selectionChanged,
+        this, &MainWindow::updateActions);
 
+}
+
+void MainWindow::updateActions(const QItemSelection &selection)
+{
+    QModelIndexList indexes = selection.indexes();
+
+    if (!indexes.isEmpty()) {
+        removeAction->setEnabled(true);
+        editAction->setEnabled(true);
+    } else {
+        removeAction->setEnabled(false);
+        editAction->setEnabled(false);
+    }
 }
 
 
