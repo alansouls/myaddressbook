f6015f78e6a67615c2bb4c4419805209b8dc6b87
= <=>
2019-02-01 14:20:15 -0300

    Advanced on Table Model

--- tablemodel.h
+++ tablemodel.h
@@ -1,11 +1,24 @@
 #ifndef TABLEMODEL_H
 #define TABLEMODEL_H
+#include <QAbstractTableModel>
+#include <QList>
 
+struct Contact{
+    QString name;
+    QString address;
+};
 
-class TableModel
+class TableModel : public  QAbstractTableModel
 {
 public:
-    TableModel();
+    TableModel(QObject *parent = nullptr);
+    TableModel(QList<Contact> contacts,QObject *parent = nullptr);
+
+    int rowCount(const QModelIndex &parent) const override;
+    int columnCount(const QModelIndex &parent) const override;
+    bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole) override;
+    bool insertRows(int position, int rows, const QModelIndex &index = QModelIndex()) override;
+    bool removeRows(int position, int rows, const QModelIndex &index = QModelIndex()) override;
 };
 
 #endif // TABLEMODEL_H
